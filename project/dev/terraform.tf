5e8315b2768c8cf33b1bf016450b7d66fea6509b
Vikash Kumar <30976812+VikasDz@users.noreply.github.com>
2024-04-26 19:40:05 +0530

    Delete project directory

--- project/dev/terraform.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "tws-terraform-state-test-d"
-    key    = "dev/terraform.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "tws-terraform-state-test-table-d"
-  }
-}
