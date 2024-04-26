de1336566f4fb4b95169582f505855817a32470f
Vikash Kumar <30976812+VikasDz@users.noreply.github.com>
2024-04-26 19:39:51 +0530

    Delete project/modules/my-app directory

--- project/modules/my-app/my_bucket.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_s3_bucket" "my_app_bucket" {
-    bucket = "${var.my_enviroment}-my-app-bucket"
-}
--- project/modules/my-app/my_table.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_dynamodb_table" "my_app_table" {
-    name = "${var.my_enviroment}-my-app-table"
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key = "id"
-    attribute {
-        name = "id"
-        type = "S"
-    }
-}
